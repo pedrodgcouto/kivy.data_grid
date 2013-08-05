028298887019b2e325b475f236e0ec680686a0a8
Pedro Couto <pedro.dgcouto@gmail.com>
2013-08-05 14:54:33 +0100

    Some new functionalities added.

--- data_grid.py
+++ data_grid.py
@@ -55,13 +55,104 @@ body_alignment = ["center", "left", "right", "right"]
 products_list = []
 
 
+counter = 0
 class DataGrid(GridLayout):
-	def add_row():
-		pass
-	def remove_row():
-		pass
-	def update_row():
-		pass
+	def add_row(self, row_data, row_align, cols_size, instance, **kwargs):
+		global counter
+		##########################################################
+		def change_on_press(self):
+			childs = self.parent.children
+			for ch in childs:
+				if ch.id == self.id:
+					print ch.id
+					print len(ch.id)
+					row_n = 0
+					if len(ch.id) == 11:
+						row_n = ch.id[4:5]
+					else:
+						row_n = ch.id[4:6]
+					for c in childs:
+						if ('row_'+str(row_n)+'_col_0') == c.id:
+							if c.state == "normal":
+								c.state="down"
+							else:	
+								c.state="normal"
+						if ('row_'+str(row_n)+'_col_1') == c.id:
+							if c.state == "normal":
+								c.state="down"
+							else:	
+								c.state="normal"
+						if ('row_'+str(row_n)+'_col_2') == c.id:
+							if c.state == "normal":
+								c.state="down"
+							else:	
+								c.state="normal"
+						if ('row_'+str(row_n)+'_col_3') == c.id:
+							if c.state == "normal":
+								c.state="down"
+							else:
+								c.state="normal"
+		def change_on_release(self):
+			if self.state == "normal":
+				self.state = "down"
+			else:
+				self.state = "normal"
+		##########################################################
+		n = 0
+		for item in row_data:
+			cell = CLabel(text=('[color=000000]' + item + '[/color]'), 
+										background_normal="background_normal.png",
+										background_down="background_pressed.png",
+										halign=row_align[n],
+										markup=True,
+										on_press=partial(change_on_press),
+										on_release=partial(change_on_release),
+										text_size=(0, None),
+										size_hint_x=cols_size[n], 
+										size_hint_y=None,
+										height=40,
+										id=("row_" + str(counter) + "_col_" + str(n)))
+			cell_width = Window.size[0] * cell.size_hint_x
+			cell.text_size=(cell_width - 30, None)
+			cell.texture_update()
+			self.add_widget(cell)
+			n+=1
+		counter += 1
+	def remove_row(self, instance, **kwargs):
+		#	removes selected rows
+		#	if none is selected, then, removes the last one
+		#check if are selected rows
+		childs = self.parent.children
+		for ch in childs:
+			for c in ch.children:
+				if c.id != "Header_Label":
+					if c.state == "down":
+						self.remove_widget(c)
+						print str(c.id) + '   -   ' + str(c.state)
+
+			
+
+	def select_all(self, instance, **kwargs):
+		childs = self.parent.children
+		for ch in childs:
+			for c in ch.children:
+				if c.id != "Header_Label":
+					c.state = "down"
+
+	def unselect_all(self, instance, **kwargs):
+		childs = self.parent.children
+		for ch in childs:
+			for c in ch.children:
+				if c.id != "Header_Label":
+					c.state = "normal"
+
+	def show_log(self, instance, **kwargs):
+		childs = self.parent.children
+		for ch in childs:
+			for c in ch.children:
+				if c.id != "Header_Label":
+					print str(c.id) + '   -   ' + str(c.state) +  '   -   ' + str(c.text)
+		
 	def __init__(self, header_data, body_data, b_align, cols_size, **kwargs):
 		super(DataGrid, self).__init__(**kwargs)
 		self.size_hint_y=None
@@ -76,88 +167,96 @@ class DataGrid(GridLayout):
 																	markup=True, 
 																	size_hint_y=None,
 																	height=40,
+																	id="Header_Label",
 																	size_hint_x=cols_size[n]))
 			n+=1
-		counter = 0
-		for bcell in body_data:
-			n = 0
-			for item in bcell:
-				def change_on_press(self):
-					childs = self.parent.children
-					for ch in childs:
-						if ch.id == self.id:
-							print ch.state
-							print len(ch.id)
-							row_n = 0
-							if len(ch.id) == 11:
-								row_n = ch.id[4:5]
-							else:
-								row_n = ch.id[4:6]
-							for c in childs:
-								if ('row_'+str(row_n)+'_col_0') == c.id:
-									if c.state == "normal":
-										c.state="down"
-									else:	
-										c.state="normal"
-								if ('row_'+str(row_n)+'_col_1') == c.id:
-									if c.state == "normal":
-										c.state="down"
-									else:	
-										c.state="normal"
-								if ('row_'+str(row_n)+'_col_2') == c.id:
-									if c.state == "normal":
-										c.state="down"
-									else:	
-										c.state="normal"
-								if ('row_'+str(row_n)+'_col_3') == c.id:
-									if c.state == "normal":
-										c.state="down"
-									else:
-										c.state="normal"
-				def change_on_release(self):
-					if self.state == "normal":
-						self.state = "down"
-					else:
-						self.state = "normal"
-				#Cell definition
-				cell = CLabel(text=('[color=000000]' + item + '[/color]'), 
-											background_normal="background_normal.png",
-											background_down="background_pressed.png",
-											halign=b_align[n],
-											markup=True,
-											on_press=partial(change_on_press),
-											on_release=partial(change_on_release),
-											text_size=(0, None),
-											size_hint_x=cols_size[n], 
-											size_hint_y=None,
-											height=40,
-											id=("row_" + str(counter) + "_col_" + str(n)))
-				cell_width = Window.size[0] * cell.size_hint_x
-				cell.text_size=(cell_width - 30, None)
-				cell.texture_update()
-				self.add_widget(cell)
-				n+=1
-			counter += 1
+		# counter = 0
+		# for bcell in body_data:
+		# 	n = 0
+		# 	for item in bcell:
+		# 		def change_on_press(self):
+		# 			childs = self.parent.children
+		# 			for ch in childs:
+		# 				if ch.id == self.id:
+		# 					print ch.state
+		# 					print len(ch.id)
+		# 					row_n = 0
+		# 					if len(ch.id) == 11:
+		# 						row_n = ch.id[4:5]
+		# 					else:
+		# 						row_n = ch.id[4:6]
+		# 					for c in childs:
+		# 						if ('row_'+str(row_n)+'_col_0') == c.id:
+		# 							if c.state == "normal":
+		# 								c.state="down"
+		# 							else:	
+		# 								c.state="normal"
+		# 						if ('row_'+str(row_n)+'_col_1') == c.id:
+		# 							if c.state == "normal":
+		# 								c.state="down"
+		# 							else:	
+		# 								c.state="normal"
+		# 						if ('row_'+str(row_n)+'_col_2') == c.id:
+		# 							if c.state == "normal":
+		# 								c.state="down"
+		# 							else:	
+		# 								c.state="normal"
+		# 						if ('row_'+str(row_n)+'_col_3') == c.id:
+		# 							if c.state == "normal":
+		# 								c.state="down"
+		# 							else:
+		# 								c.state="normal"
+		# 		def change_on_release(self):
+		# 			if self.state == "normal":
+		# 				self.state = "down"
+		# 			else:
+		# 				self.state = "normal"
+		# 		#Cell definition
+		# 		cell = CLabel(text=('[color=000000]' + item + '[/color]'), 
+		# 									background_normal="background_normal.png",
+		# 									background_down="background_pressed.png",
+		# 									halign=b_align[n],
+		# 									markup=True,
+		# 									on_press=partial(change_on_press),
+		# 									on_release=partial(change_on_release),
+		# 									text_size=(0, None),
+		# 									size_hint_x=cols_size[n], 
+		# 									size_hint_y=None,
+		# 									height=40,
+		# 									id=("row_" + str(counter) + "_col_" + str(n)))
+		# 		cell_width = Window.size[0] * cell.size_hint_x
+		# 		cell.text_size=(cell_width - 30, None)
+		# 		cell.texture_update()
+		# 		self.add_widget(cell)
+		# 		n+=1
+		# 	counter += 1
 
 
 
 grid = DataGrid(header, data, body_alignment, col_size)
 
-scroll = ScrollView(size_hint=(1, 1), size=(400, 400), pos_hint={'center_x':.5, 'center_y':.5})
+scroll = ScrollView(size_hint=(1, 1), size=(400, 500000), pos_hint={'center_x':.5, 'center_y':.5})
 scroll.add_widget(grid)
 scroll.do_scroll_y = True
 scroll.do_scroll_x = False
 
 
-
-add_row_btn = Button(text="Add Row")
-del_row_btn = Button(text="Delete Row")
+pp = partial(grid.add_row, ['001', 'Teste', '4.00', '4.00'], body_alignment, col_size)
+add_row_btn = Button(text="Add Row", on_press=pp)
+del_row_btn = Button(text="Delete Row", on_press=partial(grid.remove_row))
 upt_row_btn = Button(text="Update Row")
+slct_all_btn = Button(text="Select All", on_press=partial(grid.select_all))
+unslct_all_btn = Button(text="Unselect All", on_press=partial(grid.unselect_all))
+
+show_grid_log = Button(text="Show log", on_press=partial(grid.show_log))
 
 btn_grid = BoxLayout(orientation="vertical")
 btn_grid.add_widget(add_row_btn)
 btn_grid.add_widget(del_row_btn)
 btn_grid.add_widget(upt_row_btn)
+btn_grid.add_widget(slct_all_btn)
+btn_grid.add_widget(unslct_all_btn)
+btn_grid.add_widget(show_grid_log)
 
 root = BoxLayout(orientation="horizontal")
 
